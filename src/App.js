8bdfef53cf48ef4234296e78540327cba22d9862
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-18 06:35:14 +0430

    Components added to App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  Container
+} from "@chakra-ui/react";
+
+
+
+import { Header, NavMenu, CardList  } from "./components";
+
+export default function App() {
+
+
+  const maxWith = "850px"
+
+  return (
+    <>
+      <NavMenu maxWith={maxWith} />
+      <Container maxW={maxWith} mt={10} mb={10}>
+        <Header />
+
+        <CardList cardWith={"sm"} cardTitle="Articals" />
+
+
+        <CardList cardWith={"4xl"} cardTitle="Projects" />
+
+
+      </Container>
+    </>
+  );
+}
+
