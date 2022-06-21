1ed9c684b9eceb560400b9ec70f38b1edda3bd35
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-06-22 02:39:01 +0430

    Dark MODE

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode='dark' />
+      <ColorModeScript initialColorMode='Dark' />
       <BrowserRouter>
         <App />
       </BrowserRouter>
